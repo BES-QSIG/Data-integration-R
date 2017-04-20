023658f6bbd239ac129d50c9f367e5535822cc12
NERC\susjar <susjar@ceh.ac.uk>
2017-04-20 11:17:51 +0100

    Added more habitat areas

--- 2. Visualising and exploring data.R
+++ 2. Visualising and exploring data.R
@@ -28,7 +28,17 @@ cs_data <- subset(cs_data, select = -c(YEAR,ID,LAND_CLASS90,EZ_DESC_07))
 
 boxplot(cs_data$AREA ~ cs_data$BROAD_HABITAT)
 
-#this is very messy - calculate a more useful statistic - priority area
+#this is very messy - lets focus on a few habitats and group priority habitats together in one category. Habitats to focus on - arable, improved grassland and woodland - expect these to be related to agricultural intensity from ag census
+
+#create columns for each habitat
+cs_data$arable_area <- 0 #create empty column to start
+cs_data$arable_area[cs_data$BROAD_HABITAT == 4] <- cs_data$AREA[cs_data$BROAD_HABITAT == 4]
+
+cs_data$impgrass_area <- 0 #create empty column to start
+cs_data$impgrass_area[cs_data$BROAD_HABITAT == 5] <- cs_data$AREA[cs_data$BROAD_HABITAT == 5]
+
+cs_data$woodland_area <- 0 #create empty column to start
+cs_data$woodland_area[cs_data$BROAD_HABITAT %in% c(1,2)] <- cs_data$AREA[cs_data$BROAD_HABITAT %in% c(1,2)] # consider both broadleaf (1) and coniferous (2) woodland
 
 #flag up priority habitats
 cs_data$priority <- 0
@@ -37,7 +47,7 @@ cs_data$priority[cs_data$BROAD_HABITAT %in% 24:45] <- 1
 cs_data$priority_area <- cs_data$AREA*cs_data$priority
 
 #calculate area of priority habitat per square
-cs_areas <- aggregate(priority_area ~ SQUARE + COUNTRY + COUNTY, data = cs_data,FUN = sum)
+cs_areas <- aggregate(cbind(arable_area,impgrass_area,woodland_area,priority_area) ~ SQUARE + COUNTRY + COUNTY, data = cs_data,FUN = sum)
 
 #add (false) locations where SQUARE column matches between data sets
 
@@ -45,18 +55,19 @@ cs_areas$EASTING <- cs_locs$Easting[match( cs_areas$SQUARE,cs_locs$SQUARE)]
 cs_areas$NORTHING <- cs_locs$Northing[match( cs_areas$SQUARE,cs_locs$SQUARE)]
 
 
-#plot average priority area per country
+#plot average arable area per country
 
-boxplot(cs_areas$priority_area ~ cs_areas$COUNTRY)
+boxplot(cs_areas$arable_area ~ cs_areas$COUNTRY)
 
 #plot by county - what is obvious?
 
 #histogram
 
-hist(cs_areas$priority_area)
+hist(cs_areas$arable_area)
 
 #what does the data distribution look like?
 
+#what about other habitat areas (improved grassland, woodland and priority habitats)?
 
 #map the data
 
@@ -66,9 +77,12 @@ blighty()
 points(cs_areas$EASTING/1000, cs_areas$NORTHING/1000, cex = 1, pch = 20, col = "red")
 #note the nonsense locations so some might be in the sea!
 
-#change point size by area of habitat
+#change point colour by area of habitat
 blighty()
-points(cs_areas$EASTING/1000, cs_areas$NORTHING/1000, cex = ceiling(cs_areas$priority_area/500000)+1, pch = 20, col = ceiling(cs_areas$priority_area/500000)+1)
+points(cs_areas$EASTING/1000, cs_areas$NORTHING/1000, pch = 20, col = ceiling(cs_areas$arable_area/100000)+1)
+legend(550,1200, c("0-0.09", "0.1-0.19","0.2-0.29","0.3-0.39","0.4-0.49","0.5-0.59","0.6-0.69","0.7-0.79","0.8-0.89","0.9-0.99", "1"), col = 1:11, pch = 20, cex = 0.6, title = "Arable land (km2)")
+             
+      
 #not much spatial pattern due to false locations!
 
 
@@ -93,10 +107,27 @@ points(ag_data$x/1000, ag_data$y/1000)
 
 library(raster)
 
-#look at woodland area
-ag_raster_df <- ag_data[,c(1,2,7)]
+#look at total area of holdings
+ag_raster_df <- ag_data[,c(1,2,4)]
 
 coordinates(ag_raster_df) <- ~x+y
 gridded(ag_raster_df) <- TRUE
 ag_raster <- raster(ag_raster_df)
 plot(ag_raster)
+
+#get information about raster
+ag_raster
+
+#note resolution - 2km by 2km 
+
+#what is resolution of CS data? 1km sample but 10km spatial resolution - not clear how to process. Need background info and understanding
+
+#for following example decide to average ag census data for 10km described by survey square?
+
+
+
+
+
+
+
+
